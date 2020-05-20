6dd87ecdad5e6b200e8724c4995055003d5629d0
BGD <56988862+BanDuong@users.noreply.github.com>
2020-05-20 22:41:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,53 +37,107 @@ class Application(Frame):
         self.en_pass.pack()
         self.en_pass.place(x=77, y=76)
 
-        self.bt_login=Button(t,text="Log in",font=(13),height=2,command=self.Call) ############################# Button Login
+        self.bt_login=Button(t,text="Log in",font=(13),height=2,command=self.Login) # Button Login
         self.bt_login.pack()
         self.bt_login.place(x=220,y=46)
 
-        self.bt_signup = Button(t,text="Sign up",command=self.Creat_Sigup) ####################### Button Sign up
+        self.bt_signup = Button(t,text="Sign up",command=self.SigUp) # Button Sign up
         self.bt_signup.pack()
         self.bt_signup.place(x=77, y=100)
 
+    def SigUp(self): # Process Sign Up
 
-        def Creat_Sigup(self):
-        top=Toplevel(t)
-        self.frame = top.geometry("300x140+500+180")
-        top.resizable(width=False, height=False)
+        self.top=Toplevel(t)
+        self.frame = self.top.geometry("300x180+450+180")
+        self.top.resizable(width=False, height=False)
+        self.lb2 = Label(self.top, text="--Register--", font=("", 14)).pack()
 
-        self.lb = Label(top, text="Sign Up", font=("", 14)).pack()
+        self.lb1_acc = Label(self.top, text="account: ")  # <p> account
+        self.lb1_acc.pack()
+        self.lb1_acc.place(x=20, y=50)
 
-        self.lb_acc = Label(top, text="account: ")  # <p> account
-        self.lb_acc.pack()
-        self.lb_acc.place(x=20, y=50)
+        self.en1_acc = Entry(self.top)  # Entry account
+        self.en1_acc.pack()
+        self.en1_acc.place(x=77, y=50)
+        self.en1_acc.focus()
 
-        self.en_acc = Entry(top)  # Entry account
-        self.en_acc.pack()
-        self.en_acc.place(x=77, y=50)
+        self.lb1_pass = Label(self.top, text="password: ")  # <p> password
+        self.lb1_pass.pack()
+        self.lb1_pass.place(x=20, y=76)
 
-        self.lb_pass = Label(top, text="password: ")  # <p> password
-        self.lb_pass.pack()
-        self.lb_pass.place(x=20, y=76)
+        self.en1_pass = Entry(self.top,show="*")  # Entry password
+        self.en1_pass.pack()
+        self.en1_pass.place(x=77, y=76)
 
-        self.en_pass = Entry(top, show="*")  # Entry password
-        self.en_pass.pack()
-        self.en_pass.place(x=77, y=76)
+        self.lb1_conf = Label(self.top, text="confirm: ")  # <p> confirm
+        self.lb1_conf.pack()
+        self.lb1_conf.place(x=20, y=100)
 
-        self.lb_conf = Label(top, text="confirm: ")  # <p> confirm
-        self.lb_conf.pack()
-        self.lb_conf.place(x=20, y=100)
+        self.en1_conf = Entry(self.top,show="*")  # Entry confirm
+        self.en1_conf.pack()
+        self.en1_conf.place(x=77, y=100)
 
-        self.en_conf = Entry(top, show="*")  # Entry confirm
-        self.en_conf.pack()
-        self.en_conf.place(x=77, y=100)
+        self.lb1_phone = Label(self.top, text="phone: ")  # <p> confirm
+        self.lb1_phone.pack()
+        self.lb1_phone.place(x=20, y=126)
 
-        self.bt_login = Button(top, text="Submit", font=(13), height=2)  ############### Button Submit
+        self.en1_phone = Entry(self.top)  # Entry confirm
+        self.en1_phone.pack()
+        self.en1_phone.place(x=77, y=126)
+
+        self.bt_login = Button(self.top, text="Submit", font=(13), height=2,command=self.Storage)  # Button Submit
         self.bt_login.pack()
         self.bt_login.place(x=220, y=70)
-        
-    def Call(self):
+
+    def Interface_Call(self): # Process Login
+        t.withdraw() # hidden main window
+        self.lg=Toplevel(t)
+        self.frame = self.lg.geometry("300x140+500+180")
+        self.lg.resizable(width=False, height=False)
+        self.lb = Label(self.lg, text="CaVid-G4 App", font=("", 14)).pack()
 
 
+
+        self.bt_logout = Button(self.lg, text="Logout", height=3, width=7, command=self.LogOut)
+        self.bt_logout.pack(side="left")
+
+        self.bt_exit = Button(self.lg, text="Exit", height=3, width=7,command=t.destroy)
+        self.bt_exit.pack(side="right")
+
+        self.bt_call = Button(self.lg, text="Call", height=3,width=7)  ############################################ Button Call
+        self.bt_call.pack()
+
+    def LogOut(self): # Process Logout
+        t.deiconify() # show main window again
+        self.lg.destroy()
+
+#-------------------------------Store and process data------------------------------------------------------------------
+
+    def Storage(self): # Store data at a Repository.txt file
+        infor=self.en1_acc.get()+" " + self.en1_pass.get() + " " + self.en1_phone.get()+"\n"
+        f=open("Repository.txt","a")
+        f.writelines(infor)
+        f.close()
+        messagebox.showinfo("Notification","Register Access!")
+        self.top.destroy()
+
+    def Login(self):
+        data=[]
+        f=open("Repository.txt","r")
+        while True:
+            infor=f.readline()
+            if(infor==""):
+                messagebox.showwarning("Warring!","Account does not exist")
+                break
+            else:
+                data=infor.split()
+                if(self.en_acc.get()==data[0] and self.en_pass.get()==data[1]):
+                    self.Interface_Call()
+                    break
+                elif(self.en_acc.get()!=data[0] or self.en_pass.get()!=data[1]):
+                    messagebox.showerror("Error!", "incorrect account or password")
+                    break
+        f.close()
 
 if __name__=="__main__":
     t=Tk()
